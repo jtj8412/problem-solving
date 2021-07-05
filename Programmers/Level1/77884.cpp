e56254cd65bd20959419093e29b88a9408574fc6
jtj8412 <jtj8412@gmail.com>
2021-07-05 16:18:13 +0900

    Programmers Level1

---
+++ Programmers/Level1/77884.cpp
@@ -0,0 +1,48 @@
+/* ¾à¼öÀÇ °³¼ö¿Í µ¡¼À */
+
+#include <string>
+#include <vector>
+
+#define MAX_LENGTH 1000
+
+using namespace std;
+
+vector<int>* get_prime(int right) {
+    vector<int>* prime_vec = new vector<int>();
+    bool not_prime[MAX_LENGTH + 1] = {};
+    
+    for (int i = 2; i <= right; ++i) {
+        if (not_prime[i]) continue;
+
+        for (int j = 2; i * j <= right; ++j) {
+            not_prime[i * j] = true;
+        }
+        prime_vec->push_back(i);
+    }
+    return prime_vec;
+}
+
+int solution(int left, int right) {
+    int answer = 0;
+    vector<int>* prime_vec = get_prime(right);
+    for (int i = left; i <= right; ++i) {
+        int num = i;
+        int mul = 1;
+        vector<int> divisor_vec; 
+        for (int j = 0; j < prime_vec->size(); ++j) {
+            int prime = (*prime_vec)[j], cur = prime, k;
+            if (prime > num) break;
+            for (k = 1; num % cur == 0; ++k, cur*=prime);
+            if (k != 1) divisor_vec.push_back(k - 1);
+        }
+        for (int j = 0; j < divisor_vec.size(); ++j) {
+            mul *= (divisor_vec[j] + 1);
+        }
+        if (mul % 2 == 0) answer += num;
+        else answer -= num;
+    }
+
+    delete(prime_vec);
+
+    return answer;
+}
