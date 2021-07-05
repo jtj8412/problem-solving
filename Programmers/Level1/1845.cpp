8275fdeab5b417edb0ad7dec1360f9c1a5723d1e
jtj8412 <jtj8412@gmail.com>
2021-07-05 16:33:58 +0900

    Programmers Level1

---
+++ Programmers/Level1/1845.cpp
@@ -0,0 +1,24 @@
+/* ÆùÄÏ¸ó */
+
+#include <vector>
+
+#define MAX_NUM 200000
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0, cnt = 0, cache[MAX_NUM] = {}, select = nums.size() / 2;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        int num = nums[i];
+        
+        if (cache[num] != 0) continue;
+
+        cache[num] = 1;
+        cnt++;
+    }
+
+    answer = (select > cnt) ? cnt : select;
+    return answer;
+}
