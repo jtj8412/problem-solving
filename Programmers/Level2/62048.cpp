5316fb3d0ddc60bf0f342ab86dc59484220080be
jtj8412 <jtj8412@gmail.com>
2021-07-11 15:56:01 +0900

    Programmers Level2

--- Programmers/Level2/62048.cpp
+++ Programmers/Level2/62048.cpp
@@ -1,19 +1,27 @@
 /* 멀쩡한 사각형 */
 
+#include <math.h>
+
 using namespace std;
 
-int GCD(int a, int b) {
-    while (b != 0) {
-        int r = a % b;
-        a = b;  
-        b = r;
-    }
-    return a;
-}
-
-
 long long solution(int w,int h) {
-    int gcd = GCD(w, h);
-    int w_ratio = w / gcd, h_ratio = h / gcd;
-    return 0;
-} 
+    double half_area = (double)w * h * 0.5,
+        area = (double)w * h, 
+        full = 0, ratio = 0, len = 0;
+
+    if (w > h) {
+        ratio = (double)h / w;
+        len = w;
+    } else if (w < h) {
+        ratio = (double)w / h;
+        len = h;
+    } else {
+        return area - w;
+    }
+
+    for (int i = 1; i <= len; ++i) {
+        full += ceil(ratio * i);
+    }
+
+    return area - (full - half_area) * 2;
+}
