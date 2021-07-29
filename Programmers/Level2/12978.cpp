6ff6a11eb983991bd734bff72fbc2b4f877c3bc2
jtj8412 <jtj8412@gmail.com>
2021-07-29 20:46:02 +0900

    Programmers Level2

---
+++ Programmers/Level2/12978.cpp
@@ -0,0 +1,37 @@
+/* 배달 */
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int route[51][51] = {}, cache[51], N, K;
+vector<int> route_vec[51];
+
+void logic(int idx = 1, int sum = 0, long long visited = 1 << 1) {
+    for (auto i : route_vec[idx]) {
+        // if (visited & (1 << i) || sum + route[idx][i] > K || sum + route[idx][i] >= cache[i]) continue;
+        // cache[i] = sum + route[idx][i];
+        if (visited & ((long long)1 << i) || sum + route[idx][i] > K) continue;
+        cache[i] = min(cache[i], sum + route[idx][i]);
+        logic(i, cache[i], visited | ((long long)1 << i));
+    }
+}
+
+int solution(int n, vector<vector<int>> road, int k) {
+    int answer = 1, i;
+    N = n; K = k;
+
+    fill_n(cache, N + 1, 500001);
+    fill_n((int*)route, 51 * 51, 10001);
+    for (i = 0; i < road.size(); ++i) {
+        route[road[i][0]][road[i][1]] = route[road[i][1]][road[i][0]] = min(route[road[i][0]][road[i][1]], road[i][2]);
+        route_vec[road[i][0]].push_back(road[i][1]);
+        route_vec[road[i][1]].push_back(road[i][0]);
+    }
+    logic();
+
+    for (i = 1; i <= N; ++i) {
+        if (cache[i] <= K) ++answer;
+    }
+    return answer;
+}
