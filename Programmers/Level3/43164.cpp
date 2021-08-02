61f4afd84540b50cc0f269e5ca817a2e10540f02
jtj8412 <jtj8412@gmail.com>
2021-08-03 00:10:50 +0900

    Programmers Level3

--- Programmers/Level3/43164.cpp
+++ Programmers/Level3/43164.cpp
@@ -2,18 +2,32 @@
 
 #include <string>
 #include <vector>
-#include <iostream>
-
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
-vector<string> solution(vector<vector<string>> tickets) {
-    vector<string> answer;
-    return answer;
+unordered_map<string, vector<pair<string, bool>>> dict;
+vector<string> answer;
+int N;
+
+bool logic(const string &key, int cnt = 0) {
+    if (cnt == N) return true;
+    for (int i = 0; i < dict[key].size(); ++i) {
+        if (dict[key][i].second) continue;
+        answer.push_back(dict[key][i].first);
+        dict[key][i].second = true;
+        if (logic(dict[key][i].first, cnt + 1)) return true;
+        answer.pop_back();
+        dict[key][i].second = false;
+    }
+    return false;
 }
 
-int main() {
-    vector<vector<string>> vvec = {{"ICN", "JFK"},{"HND", "IAD"},{"JFK", "HND"}};
-    for (auto s : solution(vvec)) {
-        cout << s << " " << endl;
-    }
+vector<string> solution(vector<vector<string>> tickets) {
+    N = tickets.size();
+    for (auto ticket : tickets) dict[ticket[0]].push_back(make_pair(ticket[1], false));
+    for (auto kv : dict) sort(dict[kv.first].begin(), dict[kv.first].end());
+    answer.push_back("ICN");
+    logic("ICN");
+    return answer;
 }
