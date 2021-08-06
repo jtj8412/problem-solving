865a3aea719a341ce1ff926bf26b2b8762810f0b
jtj8412 <jtj8412@gmail.com>
2021-08-06 23:07:18 +0900

    Programmers Level3

--- Programmers/Level3/12946.cpp
+++ Programmers/Level3/12946.cpp
@@ -1,4 +1,12 @@
-/* 하노이의 탑 */
+/* 
+[ 하노이의 탑 ]
+
+- 나의 해설
+원판 cnt개를 from에서 to로 옮기기
+1. from에서 원판 cnt-1개를 임시위치(not from, not to)로 이동 (재귀)
+2. from에 남은, 가장 큰 원판 1개를 to로 이동
+3. 임시위치로 옮긴 원판 cnt-1개를 to로 이동 (재귀)
+*/
 
 #include <vector>
 using namespace std;
