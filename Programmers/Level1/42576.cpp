5f2f1759d081752fbf0b8e40c334c61596b6b4dd
jtj8412 <jtj8412@gmail.com>
2021-08-11 09:06:24 +0900

    Update Programmers Level1 Comments

--- Programmers/Level1/42576.cpp
+++ Programmers/Level1/42576.cpp
@@ -1,38 +1,27 @@
 //----------------------------------------------------------------------------------
 // [ 완주하지 못한 선수 ] https://programmers.co.kr/learn/courses/30/lessons/42576
+//
+// 1. 해시맵에 참가자(participant) 및 동명이인 수 저장
+// 2. 완주자(completion)를 key로 해시맵에 있는 참가자 탐색 및 감소
+// 3. 2결과 0이되면 해시맵에서 삭제
+// 4. 해시맵에 유일하게 남은 참가자가 '완주하지 못한 선수'
 //----------------------------------------------------------------------------------
 
 #include <string>
 #include <vector>
 #include <unordered_map>
-
 using namespace std;
 
-string solution(vector<string> participant, vector<string> completion) 
-{
-	string ans = "";
-	unordered_map<string, int> par_map;
-	unordered_map<string, int> com_map;
+string solution(vector<string> participant, vector<string> completion)  {
+	unordered_map<string, int> dict;
+	unordered_map<string, int>::iterator iter;
 
-	int size = completion.size();
-
-	for (int i = 0; i < size; ++i)
-		com_map[completion[i]] += 1;
-
-	size++;
-
-	for (int i = 0; i < size; ++i)
-		par_map[participant[i]] += 1;
-
-	for (int i = 0; i < size; ++i) {
-		unordered_map<string, int>::iterator iter;
-		iter = com_map.find(participant[i]);
-
-		if (iter == com_map.end() || (*iter).second != par_map[participant[i]]) {
-			ans = participant[i];
-			break;
-		}
+	for (auto s : participant) { 
+		++dict[s];
+	}
+	for (auto s : completion) {
+		if (--((iter = dict.find(s))->second) == 0) dict.erase(iter);
 	}
 
-	return ans;
+	return dict.begin()->first;
 }
--- Programmers/Level1/42748.cpp
+++ Programmers/Level1/42748.cpp
@@ -1,42 +1,23 @@
-/* K번째수 */
+//----------------------------------------------------------------------------------
+// [ k번째수 ] https://programmers.co.kr/learn/courses/30/lessons/42748
+//
+// 1. 우선순위큐에 오름차순으로 명령(commands)의 인덱스에 맞게 수를 적재
+// 2. 큐에 n번째에 위치한 수가 k번째수
+//----------------------------------------------------------------------------------
 
-#include <string>
 #include <vector>
-
+#include <queue>
 using namespace std;
-
-vector<int>* newArray(vector<int>& arr, int start, int end)
-{
-	vector<int>* ret = new vector<int>();
-
-	for (int i = start; i <= end; ++i)
-		ret->push_back(arr[i]);
-
-	return ret;
-}
-
-void sortArray(vector<int>& arr)
-{
-	int tmp;
-
-	for (unsigned int i = 0; i < arr.size() - 1; ++i){
-		for (unsigned int j = i + 1; j < arr.size(); ++j){
-			if (arr[i] < arr[j]) continue;
-
-			tmp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = tmp;
-		}
-	}
-}
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
 	vector<int> answer;
+	priority_queue<int, vector<int>, greater<int>> pq;
 
-	for (unsigned int i = 0; i < commands.size(); ++i){
-		vector<int>* arr = newArray(array, commands[i][0] - 1, commands[i][1] - 1);
-		sortArray(*arr);
-		answer.push_back((*arr)[commands[i][2] - 1]);
+	for (int i = 0, size = commands.size(); i < size; ++i) {
+		for (int j = commands[i][0] - 1; j < commands[i][1]; ++j) pq.push(array[j]);
+		for (int j = 1; j < commands[i][2]; ++j) pq.pop();
+		answer.push_back(pq.top());
+		while (!pq.empty()) pq.pop();
 	}
 
 	return answer;
